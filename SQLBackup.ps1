c1c421055512a562a8d2ab3a9c9664ae84c88099
skyrid3r698 <79767107+skyrid3r698@users.noreply.github.com>
2023-02-16 16:18:53 +0100

    Update SQLBackup.ps1
    - Initial GUI
    - Add Logging
    - Add a few logic checks

--- SQLBackup.ps1
+++ SQLBackup.ps1
@@ -1,77 +1,249 @@
-#check for admin
 $IsAdmin=[Security.Principal.WindowsIdentity]::GetCurrent()
 If ((New-Object Security.Principal.WindowsPrincipal $IsAdmin).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator) -eq $FALSE) {
-"ERROR: Das Script muss als Administrator gestartet werden!"
-pause
-exit
+    $ElevatedProcess = New-Object System.Diagnostics.ProcessStartInfo "PowerShell";
+    # Specify the current script path and name as a parameter
+    $ElevatedProcess.Arguments = "& '" + $script:MyInvocation.MyCommand.Path + "'"
+    #Set the Process to elevated
+    $ElevatedProcess.Verb = "runas"
+    #Start the new elevated process
+    [System.Diagnostics.Process]::Start($ElevatedProcess)
+    #Exit from the current, unelevated, process
+    Exit
 }
+Write-Host $(Get-Date)"Start Logging:"
+Start-Transcript -Append C:\OLA\Logs\log2.txt
+
 
 #Check if sqlcmd is installed
 $software = "Microsoft Befehlszeilenprogramme 15 für SQL Server";
 $installed = (Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* | Where { $_.DisplayName -eq $software }) -ne $null
 
 If(-Not $installed) {
-	Write-Host "'$software' bzw. SQLCmd konnte nicht gefunden werden. Die Software ist, für ein funktionierendes Backup, zwingend notwendig. Fortfahren auf eigene Verantwortung!" -ForegroundColor Red; 
+	Write-Host $(Get-Date)"[ERROR]'$software' bzw. SQLCmd konnte nicht gefunden werden. Die Software ist, für ein funktionierendes Backup, zwingend notwendig. Fortfahren auf eigene Verantwortung!" -ForegroundColor Red; 
 } else {
-	Write-Host "'$software' bzw. SQLcmd ist installiert." -ForegroundColor Green
+	Write-Host $(Get-Date)"[INFO]'$software' bzw. SQLcmd ist installiert."
 }
 
-#Download SQL Script + Anlage erforderlicher Pfade
+#Download SQL Script + Create paths
 $PathScripts = Test-Path C:\OLA\Scripts\
 $PathLogs = Test-Path C:\OLA\Logs\
-If($PathScripts -eq "True") {
-}
-else { 
-	mkdir C:\OLA\Scripts\
-	Write-Host "Skript-Pfad C:\OLA\Scripts angelegt!" -ForegroundColor Green
-}
-If($PathLogs -eq "True") {
-}
-else { 
-	mkdir C:\OLA\Logs\
-	Write-Host "Logs-Pfad C:\OLA\Logs angelegt!" -ForegroundColor Green
-}
+If($PathScripts -eq "True") {} else {mkdir C:\OLA\Scripts\ | Out-Null}
+If($PathLogs -eq "True") {} else {mkdir C:\OLA\Logs\ | Out-Null}
 Invoke-WebRequest -Uri https://ola.hallengren.com/scripts/MaintenanceSolution.sql -OutFile C:\Users\$env:USERNAME\Downloads\MaintananceSolution.sql
+# Check if paths are created
+$PathScripts = Test-Path C:\OLA\Scripts\
+$PathLogs = Test-Path C:\OLA\Logs\
+if($PathScripts -eq "True") { Write-Host $(Get-Date)"[INFO]C:\OLA\Scripts is available"} else {Write-Host $(Get-Date)"[ERROR]C:\OLA\Scripts clould not be created. Check privilege or drive letter!" -ForegroundColor Red}
+if($PathLogs -eq "True") { Write-Host $(Get-Date)"[INFO]C:\OLA\Logs is available"} else {Write-Host $(Get-Date)"[ERROR]C:\OLA\Scripts clould not be created. Check privilege or drive letter" -ForegroundColor Red}
+
+# Check if the file has been downloaded
+$PathSQLScript = Test-Path C:\Users\$env:USERNAME\Downloads\MaintananceSolution.sql
+if($PathSQLScript -eq "True") { Write-Host $(Get-Date)"[INFO]MaintananceSolution.sql successfully downloaded to" C:\Users\$env:USERNAME\Downloads\MaintananceSolution.sql} else {Write-Host $(Get-Date)"[ERROR]MaintanenceSolution.sql could not be downloaded. Check your internet connection!" -ForegroundColor Red}
 
 
 
+# GUI Window
+Add-Type -assembly System.Windows.Forms
+$main = New-Object System.Windows.Forms.Form
+$main.Text ='MSSQL Backup GUI'
+$main.Width = 800
+$main.Height = 600
+$main.AutoSize = $true
+
+#Label Title
+$LabelTitle = New-Object System.Windows.Forms.Label
+$LabelTitle.Text = "Location"
+$LabelTitle.Location  = '5,5'
+$LabelTitle.AutoSize = $true
+$LabelTitle.Font = New-Object System.Drawing.Font("Arial",12,[System.Drawing.FontStyle]::Bold)
+$main.Controls.Add($LabelTitle)
+
+# Backuplocation
+# Label Backuplocation
+$Label = New-Object System.Windows.Forms.Label
+$Label.Text = "Backup location:"
+$Label.Location  = '10,30'
+$Label.AutoSize = $true
+$main.Controls.Add($Label)
+# Textbox Backuplocation
+$textbox = New-Object System.Windows.Forms.TextBox
+$textbox.Location = '10,50'
+$textbox.Width += 200
+$main.Controls.Add($textbox)
+# Button Backuplocation
+$Button = New-Object System.Windows.Forms.Button
+$Button.Location = '310,50'
+$Button.Size = New-Object System.Drawing.Size(30,23)
+$Button.Text = "..."
+$Button.Add_Click({
+    $browse = New-Object system.windows.forms.FolderBrowserDialog
+    $script:foldername1 = 'not found'
+    if($browse.ShowDialog() -eq 'Ok'){
+        $script:foldername1 =  $textbox.Text = $browse.SelectedPath
+    }    
+})
+$main.Controls.Add($Button)
+
+# LogDir
+# Label LogDir
+$LabelLogDir = New-Object System.Windows.Forms.Label
+$LabelLogDir.Text = "Log Directory:"
+$LabelLogDir.Location  = '400,30'
+$LabelLogDir.AutoSize = $true
+$main.Controls.Add($LabelLogDir)
+# Textbox LogDir
+$textboxLogDir = New-Object System.Windows.Forms.TextBox
+$textboxLogDir.Location = '400,50'
+$textboxLogDir.Width += 200
+$textboxLogDir.Text = "C:\Program Files\Microsoft SQL Server\MSSQL13.MSSQLSERVER\MSSQL\Log"
+$main.Controls.Add($textboxLogDir)
+# Button LogDir
+$ButtonLogDir = New-Object System.Windows.Forms.Button
+$ButtonLogDir.Location = '700,50'
+$ButtonLogDir.Size = New-Object System.Drawing.Size(30,23)
+$ButtonLogDir.Text = "..."
+$ButtonLogDir.Add_Click({
+    $browseLogDir = New-Object system.windows.forms.FolderBrowserDialog
+    $script:foldername2 = 'not found'
+    if($browseLogDir.ShowDialog() -eq 'Ok'){
+        $script:foldername2 =  $textboxLogDir.Text = $browseLogDir.SelectedPath
+    }    
+})
+$main.Controls.Add($ButtonLogDir)
+
+#Label TitleSQL
+$LabelTitleSQL = New-Object System.Windows.Forms.Label
+$LabelTitleSQL.Text = "SQL Information"
+$LabelTitleSQL.Location  = '5,100'
+$LabelTitleSQL.AutoSize = $true
+$LabelTitleSQL.Font = New-Object System.Drawing.Font("Arial",12,[System.Drawing.FontStyle]::Bold)
+$main.Controls.Add($LabelTitleSQL)
+
+#User
+#Label User
+$Label1 = New-Object System.Windows.Forms.Label
+$Label1.Text = "User(has to be able to login to MSSQL):"
+$Label1.Location  = '10,125'
+$Label1.AutoSize = $true
+$main.Controls.Add($Label1)
+#Textbox User
+$textbox1 = New-Object System.Windows.Forms.TextBox
+$textbox1.Location = '10,145'
+$textbox1.Width += 50
+$textbox1.Text = "$env:USERDomain\$env:USERNAME"
+$main.Controls.Add($textbox1)
+
+#SQLDB
+#Label SQLDB
+$LabelSQLDB = New-Object System.Windows.Forms.Label
+$LabelSQLDB.Text = "SQL Datenbank"
+$LabelSQLDB.Location  = '10,180'
+$LabelSQLDB.AutoSize = $true
+$main.Controls.Add($LabelSQLDB)
+#Textbox SQLDB
+$textboxSQLDB = New-Object System.Windows.Forms.TextBox
+$textboxSQLDB.Location = '10,200'
+$textboxSQLDB.Width += 100
+$TextboxSQLDB.Text = "master"
+$main.Controls.Add($textboxSQLDB)
+
+#SQLInst
+#Label SQLInst
+$LabelSQLInst = New-Object System.Windows.Forms.Label
+$LabelSQLInst.Text = "SQL Instance"
+$LabelSQLInst.Location  = '250,180'
+$LabelSQLInst.AutoSize = $true
+$main.Controls.Add($LabelSQLInst)
+#Textbox SQLDB
+$textboxSQLInst = New-Object System.Windows.Forms.TextBox
+$textboxSQLInst.Location = '250,200'
+$textboxSQLInst.Width += 200
+$main.Controls.Add($textboxSQLInst)
+
+#Time
+#Label Time
+$LabelTime = New-Object System.Windows.Forms.Label
+$LabelTime.Text = "Time to start the Backup"
+$LabelTime.Location  = '10,300'
+$LabelTime.AutoSize = $true
+$main.Controls.Add($LabelTime)
+#Textbox Time
+$textboxTime = New-Object System.Windows.Forms.TextBox
+$textboxTime.Location = '10,325'
+$textboxTime.Width += 100
+$textboxTime.Text = "05:00"
+$main.Controls.Add($textboxTime)
+
+#Deletebackupafter
+#Label Deletebackupafter
+$Label2 = New-Object System.Windows.Forms.Label
+$Label2.Text = "Delete backup after(hours):"
+$Label2.Location  = '10,500'
+$Label2.AutoSize = $true
+$main.Controls.Add($Label2)
+#Textbox Backuplocation
+$textbox2 = New-Object System.Windows.Forms.TextBox
+$textbox2.Location = '160,500'
+$textbox2.Text = "72"
+$textbox2.Width += 150
+$main.Controls.Add($textbox2)
+
+# OK Button
+$okButton = New-Object System.Windows.Forms.Button
+$okButton.Location = New-Object System.Drawing.Point(550,500)
+$okButton.Size = New-Object System.Drawing.Size(75,23)
+$okButton.Text = 'OK'
+$okButton.DialogResult = [System.Windows.Forms.DialogResult]::OK
+$main.AcceptButton = $okButton
+$main.Controls.Add($okButton)
+
+# Cancel Button
+$cancelButton = New-Object System.Windows.Forms.Button
+$cancelButton.Location = New-Object System.Drawing.Point(675,500)
+$cancelButton.Size = New-Object System.Drawing.Size(75,23)
+$cancelButton.Text = 'Cancel'
+$cancelButton.DialogResult = [System.Windows.Forms.DialogResult]::Cancel
+$main.CancelButton = $cancelButton
+$main.Controls.Add($cancelButton)
+
+#Show GUI with result
+$result = $main.ShowDialog()
+if ($result -eq [System.Windows.Forms.DialogResult]::OK)
+{
+    Write-Host "[INFO]" $textbox1.Text
+}
+else {
+    Write-Host $(Get-Date)"[INFO]Setup cancelled!" -ForegroundColor Red
+}
 
 #backuppfad festlegen
-Write-Host "Das SQL-Skript wird nach dem Setup in C:\OLA\Scripts\MaintananceSolutionEdited.sql geschrieben und muss nach diesem Setup manuell in dem SQL-Server ausgeführt werden!" -ForegroundColor Yellow
-
-$location = Read-Host 'Backupziel(Schreibweise D:\ELOBACKUP\BACKUP)'
-$deletebackupafter = Read-Host 'Nach wie vielen Stunden soll das Backup gelöscht werden (Schreibweise 72)'
-$PathBackup = Test-Path $location
-If($PathBackup -eq "True") {
+Write-Host $(Get-Date)"[WARN]The SQL Script is going to be written to C:\OLA\Scripts\MaintananceSolutionEdited.sql and has to be run manually after creation" -ForegroundColor Yellow
+$username = $textbox1.Text
+$deletebackupafter = $textbox2.Text
+$PathBackup = Test-Path $foldername1
+If($PathBackup -eq "True") {}
+else { mkdir $foldername1 | Out-Null
 }
-else { 
-	mkdir $location
-	Write-Host "Backup-Pfad $location angelegt!" -ForegroundColor Green
-}
+$PathBackup = Test-Path $foldername1
+if($PathBackup -eq "True") { Write-Host $(Get-Date)"[INFO]'$foldername1' is available"} else {Write-Host $(Get-Date)"[ERROR]'$foldername1' clould not be created. Check privilege or drive letter!" -ForegroundColor Red}
+$sqldatabase = $TextboxSQLDB.Text
 
 #Edit MaintananceSolution.sql
 Get-Content C:\Users\$env:USERNAME\Downloads\MaintananceSolution.sql | Foreach-Object {$_.Replace('DECLARE @BackupDirectory nvarchar(max)     = NULL', "DECLARE @BackupDirectory nvarchar(max)     = '$location'")} | Set-Content C:\Users\$env:USERNAME\AppData\Local\Temp\result.sql
 Get-Content C:\Users\$env:USERNAME\AppData\Local\Temp\result.sql | Foreach-Object {$_.Replace('DECLARE @CleanupTime int                   = NULL', "DECLARE @CleanupTime int                   = $deletebackupafter")} | Set-Content C:\OLA\Scripts\MaintananceSolutionEdited.sql
 
 #Edit and create Scripts
-$sqluser = Read-Host 'Benutzer für Anmeldung an den SQL Server (Aktuell angemeldet:'"$env:USERDomain\$env:USERNAME"')'
-if ($sqluser -eq [string]::empty) { $sqluser = "$env:USERDomain\$env:USERNAME" }
-$sqldatabase = Read-Host 'Auf welche Datenbank wird das SQL-Skript geschrieben? (Aktuell ausgewählt: master)'
-if ($sqluser -eq [string]::empty) { $sqldatabase = "master" }
-$SQLInstanz = Read-Host 'SQL-Server (Schreibweise: '"$env:COMPUTERNAME\INSTANZNAME"')'
+$SQLInstanz = $textboxSQLInst.Text
 cd C:\OLA\Scripts\
 $CommandLogCleanup = "sqlcmd -E -S $SQLInstanz -d $sqldatabase -Q ""DELETE FROM dbo.CommandLog WHERE StartTime < DATEADD(dd, -30, GETDATE());"" -b -o C:\OLA\Logs\CommandLogCleanup.txt"
 $CommandLogCleanup | out-file CommandLogCleanup.cmd -Encoding ascii
 
-$SQLLogDir = Read-Host 'LogDir des MSSQL Servers(Bsp. C:\Program Files\Microsoft SQL Server\MSSQL13.MSSQLSERVER\MSSQL\Log)'
+$SQLLogDir = $textboxLogDir.Text
 $OutFileCleanup = @"
 @echo off
-
 set _SQLLOGDIR_=$SQLLogDir
 set _LOGFILE_=C:\OLA\Logs\OutputFileCleanup.txt
-
 if exist %_LOGFILE_% del %_LOGFILE_% > nul:
-
 cmd /q /c "For /F "tokens=1 delims=" %%v In ('ForFiles /P "%_SQLLOGDIR_%" /m *_*_*_*.txt /d -30 2^>^&1') do if EXIST "%_SQLLOGDIR_%"\%%v echo del "%_SQLLOGDIR_%"\%%v& del "%_SQLLOGDIR_%"\%%v" >> %_LOGFILE_%
 "@
 $OutFileCleanup | out-file OutFileCleanup.cmd -Encoding ascii
@@ -108,7 +280,7 @@ $Monitoring | out-file Monitoring.cmd -Encoding ascii
 
 
 
-[DateTime]$Time = Read-Host 'Uhrzeit des Starts der Maintenance Skripte(Bsp.: 05:00)'
+[DateTime]$Time = $textboxTime.Text
 
 ##Create MS Tasks
 #IndexOptimizeSystemDatabases
@@ -117,7 +289,7 @@ $Monitoring | out-file Monitoring.cmd -Encoding ascii
 [string]$TaskPfad = "\OLA - SQL Mantenance Tasks"
 $TaskAusloeser = New-ScheduledTaskTrigger -Daily -DaysInterval 1 -At ($Time).tostring("HH:mm")
 $TaskAktion = New-ScheduledTaskAction -Execute "C:\OLA\Scripts\IndexOptimizeSystemDatabases.cmd"
-$TaskBenutzer = New-ScheduledTaskPrincipal -UserId "$sqluser" -RunLevel Highest
+$TaskBenutzer = New-ScheduledTaskPrincipal -UserId "$username" -RunLevel Highest
 if (Get-ScheduledTask $TaskName -ErrorAction SilentlyContinue) {Unregister-ScheduledTask $TaskName}
 Register-ScheduledTask -TaskName $TaskName -TaskPath $TaskPfad -Action $TaskAktion -Trigger $TaskAusloeser -Principal $TaskBenutzer -Description $TaskBeschrieb
 
